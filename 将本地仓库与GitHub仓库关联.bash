8cf59c4c1f6d46457baa93840c93ac399677cdea
BigBossVlin <vlin@ujoylady.com>
2025-02-13 14:54:57 +0800

    Update 将本地仓库与GitHub仓库关联.bash

--- 将本地仓库与GitHub仓库关联.bash
+++ 将本地仓库与GitHub仓库关联.bash
@@ -1,3 +1,3 @@
-git remote add origin https://github.com/BigBossVlin/VlinWebsite.git
+git remote add origin https://github.com/BigBossVlin/my-website.git
 git branch -M main
 git push -u origin main
